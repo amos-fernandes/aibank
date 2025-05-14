44f6d5775188f23ac8af8dc7a5a4909705a2762a
amos-fernandes <consultor.amos@hotmail.com>
2025-05-14 16:08:53 -0300

    fix

--- frontend/agnus/interactions.jsx
+++ frontend/agnus/interactions.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
+
+const router = useRouter();
 
 const Interactions = () => {
   const [interactions, setInteractions] = useState([]);
